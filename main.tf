11b0607b50b69720ade765419d8806e103f12e01
JuanC <jcpallaresvalenzuela@gmail.com>
2025-08-16 14:22:23 +0200

    add some config to main

--- main.tf
+++ main.tf
@@ -1,14 +1,28 @@
 # azurerm_kubernetes_cluster: incluye el default_node_pool
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
-  resource_group_name = var.resource_group_name
-  location            = "eastus"             
-  dns_prefix          = "aks-mlops-demo-dns" 
+  name                                = var.aks_cluster_name
+  resource_group_name                 = var.resource_group_name
+  location                            = "eastus"             
+  dns_prefix                          = "aks-mlops-demo-dns" 
+  image_cleaner_enabled               = true
+  image_cleaner_interval_hours        = 168
+  node_os_upgrade_channel             = "None"
+  oidc_issuer_enabled                 = true
+  workload_identity_enabled           = true
 
   default_node_pool {
-    name       = "agentpool"
-    vm_size    = "Standard_A2_v2"
-    node_count = 2
+    name                          = "agentpool"
+    vm_size                       = "Standard_A2_v2"
+    node_count                    = 2
+    auto_scaling_enabled          = true
+    max_count                     = 5
+    min_count                     = 2
+
+    upgrade_settings {
+      drain_timeout_in_minutes      = 0
+      max_surge                     = "10%"
+      node_soak_duration_in_minutes = 0
+    }
   }
 
   identity {
